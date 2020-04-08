f3f4e7158581edc0af08f3cd252a2b3c6573c31d
wojiaochadi <264835229@qq.com>
2020-04-08 14:56:35 +0800

    配置Spring整合Mybatis关键配置

--- Mybatis_06_ssm/src/main/java/com/atguigu/mybatis/dao/EmployeeMapper.java
+++ Mybatis_06_ssm/src/main/java/com/atguigu/mybatis/dao/EmployeeMapper.java
@@ -2,7 +2,12 @@ package com.atguigu.mybatis.dao;
 
 import com.atguigu.mybatis.bean.Employee;
 
+import java.util.List;
+
 public interface EmployeeMapper {
 
-	public Employee getEmpById(Integer id);
+    public Employee getEmpById(Integer id);
+
+    public List<Employee> getEmps();
+
 }
