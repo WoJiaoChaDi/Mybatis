d8ac43922b181ac695a76082fda7228d6fd456d7
wojiaochadi <264835229@qq.com>
2020-04-08 15:40:11 +0800

    Mybat的逆向工程 - MybatisGenerator

--- Mybatis_07_MBG_MybatisGenerator/src/main/java/com/atguigu/mybatis/test/MyBatisTest.java
+++ Mybatis_07_MBG_MybatisGenerator/src/main/java/com/atguigu/mybatis/test/MyBatisTest.java
@@ -24,16 +24,4 @@ public class MyBatisTest {
 		return new SqlSessionFactoryBuilder().build(inputStream);
 	}
 
-	@Test
-	public void testMbg() throws Exception {
-		List<String> warnings = new ArrayList<String>();
-		boolean overwrite = true;
-		File configFile = new File("mbg.xml");
-		ConfigurationParser cp = new ConfigurationParser(warnings);
-		Configuration config = cp.parseConfiguration(configFile);
-		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
-		MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
-				callback, warnings);
-		myBatisGenerator.generate(null);
-	}
 }
